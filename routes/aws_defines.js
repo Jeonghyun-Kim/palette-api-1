a123c01090b59e65986fbcbd05004478cc695b0a
Kay Kim <kimjh@bawi.org>
2020-05-25 22:05:55 +0900

    painting upload api use multer instead of multer-s3 to customize add streamify module

--- routes/aws_defines.js
+++ routes/aws_defines.js
@@ -1,7 +1,4 @@
 const AWS = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const path = require('path');
 
 const BUCKETS = {
   PAINTING: 'palette-painting-s3',
@@ -15,21 +12,7 @@ AWS.config.update({region: 'ap-northeast-2'});
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-const getMulterS3 = (fileName, bucketName) => {
-  return multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: bucketName,
-      acl: 'public-read',
-      key: (req, file, cb) => {
-        const extension = path.extname(file.originalname);
-        cb(null, fileName + extension);
-      }
-    })
-  })
-}
-
 module.exports = {
   BUCKETS,
-  getMulterS3
+  s3
 }
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,16 @@ const express = require('express');
 const router = express.Router();
 const { uuid } = require('uuidv4');
 const sha256 = require('sha256');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+const streamifier = require('streamifier');
+const upload = multer({ storage: multer.memoryStorage() });
 
 const logger = require('../config/winston_config');
 const { User, Painting } = require('../models');
 const { verifyToken } = require('./middlewares');
-const { getMulterS3, BUCKETS } = require('./aws_defines');
+const { s3, BUCKETS } = require('./aws_defines');
 const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../status_code');
 
 const version = '0.0.1';
@@ -30,27 +35,45 @@ router.get('/user/my', verifyToken, async (req, res, next) => {
     logger.error(`[/user/my] ${error}`);
 
     return next(error);
-  }
+  };
 });
 
-router.post('/painting', verifyToken, async (req, res, next) => {
+router.post('/painting', verifyToken, upload.single('painting'), async (req, res, next) => {
   const fileName = sha256(uuid());
-  const upload = getMulterS3(fileName, BUCKETS.PAINTING);
   const { painter, name, description, material, width, height, price, onSale } = req.body;
   // TODO: Input Validation
+  const extension = path.extname(req.file.originalname);
+  logger.info(`FILENAME: ${fileName}${extension}`);
   try {
-    await Painting.create({
+    const painting = await Painting.create({
       painter, name, description, material, width, height, price, onSale,
       ownerId: req.id,
       src: fileName
     });
+    const fileStream = streamifier.createReadStream(req.file.buffer);
+    s3.upload({
+      Bucket: BUCKETS.PAINTING,
+      Key: fileName + extension,
+      Body: fileStream,
+      ACL: 'public-read'
+    }, async (err, data) => {
+      if (err) {
+        logger.error(`[AWS UPLOAD PAINTING] ${err}`);
+        await Painting.destroy({ where: { id: painting.id } });
+
+        return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json({ error: DB_STATUS_CODE.AWS_S3_ERROR });
+      } if (data) {
+        logger.info(`[PAINTING UPLOADED SUCCESSFULLY]`);
+
+        return res.status(HTTP_STATUS_CODE.CREATED).json({ error: DB_STATUS_CODE.OK });
+      }
+    })
   } catch (error) {
     logger.error(`[POST /painting] ${error}`);
+
+    return next(error);
   };
-  
-  upload.single('painting')(req, res, next);
-}, (req, res) => {
-  res.status(HTTP_STATUS_CODE.CREATED).json({ error: DB_STATUS_CODE.OK });
+  res.status(200).json({error: 0})
 });
 
 module.exports = router;
