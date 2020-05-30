92fa205b794d0be7479129cdc39e0ac1d3620085
Kay Kim <kimjh@bawi.org>
2020-05-30 16:43:52 +0900

    add put, delete methods to userRouter

--- v1/routes/userRouter.js
+++ v1/routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const sha256 = require('sha256');
 
 const logger = require('../../config/winston_config');
 const db = require('../../models');
-const { verifyToken } = require('../middlewares');
+const { verifyToken, checkAdmin } = require('../middlewares');
 const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../../status_code');
 
 router.get('/:id', verifyToken, async (req, res, next) => {
@@ -22,6 +23,102 @@ router.get('/:id', verifyToken, async (req, res, next) => {
     });
 
     return res.status(HTTP_STATUS_CODE.OK).json({ user, paintings, error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.put('/info', verifyToken, async (req, res, next) => {
+  const { username, name, gender } = req.body;
+  try {
+    const exUser = await db.User.findOne({ where: { username } });
+    if (exUser) {
+      return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({ error: DB_STATUS_CODE.USERNAME_ALREADY_OCCUPIED });
+    };
+    const user = await db.User.findOne({ where: { id: req.id } });
+    await user.update({ username, name, gender });
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.put('/password', verifyToken, async (req, res, next) => {
+  const { password, newPassword } = req.body;
+  try {
+    const user = await db.User.findOne({ where: { id: req.id }});
+    if (user.password !== sha256(password)) {
+      return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({ error: DB_STATUS_CODE.PASSWORD_WRONG });
+    };
+    await user.update({ password: sha256(password)});
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.put('/level/username/:username', verifyToken, checkAdmin, async (req, res, next) => {
+  const { level } = req.body;
+  const { username } = req.params;
+  try {
+    const user = await db.User.findOne({ where: { username } });
+    await user.update({ level });
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ level, error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.put('/level/id/:id', verifyToken, checkAdmin, async (req, res, next) => {
+  const { level } = req.body;
+  const { id } = req.params;
+  try {
+    const user = await db.User.findOne({ where: { id } });
+    await user.update({ level });
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ level, error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.delete('/', verifyToken, async (req, res, next) => {
+  const { password } = req.body;
+  try {
+    const user = await db.User.findOne({ where: { id: req.id }});
+    if (user.password !== sha256(password)) {
+      return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({ error: DB_STATUS_CODE.PASSWORD_WRONG });
+    };
+    await user.destroy();
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    logger.error(`[DB] ${error}`);
+
+    return next(error);
+  };
+});
+
+router.delete('/username/:username', verifyToken, checkAdmin, async (req, res, next) => {
+  const { username } = req.params;
+  try {
+    const user = await db.User.findOne({ where: { username } });
+    await user.destroy();
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ error: DB_STATUS_CODE.OK });
   } catch (error) {
     logger.error(`[DB] ${error}`);
 
