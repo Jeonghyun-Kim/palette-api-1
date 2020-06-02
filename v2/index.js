a909f9e504e05687bfcf20a05ded2a2bb93d2628
Kay Kim <kimjh@bawi.org>
2020-06-02 23:56:17 +0900

    bug fix

--- v2/index.js
+++ v2/index.js
@@ -11,6 +11,7 @@ const version = '0.1.1';
 
 router.use('/auth', authRouter);
 router.use('/admin', adminRouter);
+router.use('/user', userRouter);
 
 router.get('/', (_req, res) => {
   return res.status(HTTP_STATUS_CODE.OK).json({ version, error: DB_STATUS_CODE.OK });
