20b2f3f1c3d9a21294fc3ed7b5099e9ed999248a
Kay Kim <kimjh@bawi.org>
2020-06-02 22:57:15 +0900

    connect adminRouter to indexRouter

--- v2/index.js
+++ v2/index.js
@@ -7,10 +7,12 @@ const { verifyToken } = require('./middlewares');
 const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../status_code');
 
 const authRouter = require('./routes/authRouter');
+const adminRouter = require('./routes/adminRouter');
 
 const version = '0.1.1';
 
 router.use('/auth', authRouter);
+router.use('/admin', adminRouter);
 
 router.get('/', (_req, res) => {
   return res.status(HTTP_STATUS_CODE.OK).json({ version, error: DB_STATUS_CODE.OK });
