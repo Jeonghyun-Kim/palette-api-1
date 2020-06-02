81e9d10f06e94e0b5db89b0c1bf05df590341340
Kay Kim <kimjh@bawi.org>
2020-06-03 00:03:00 +0900

    bug fix

--- v2/routes/userRouter.js
+++ v2/routes/userRouter.js
@@ -10,6 +10,18 @@ const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../../status_code');
 
 router.use(jsonParser);
 
+router.get('/my', verifyToken, async (req, res) => {
+  try {
+    const user = await userUtils.findById(req.id, res);
+    
+    const { nick, name, level, email, gender, profileUrl, verified } = user;
+
+    return res.status(HTTP_STATUS_CODE.OK).json({ user: { nick, name, level, email, gender, profileUrl, verified } });
+  } catch {
+    return response.sendInternalError(res);
+  };
+});
+
 router.get('/:id', verifyToken, async (req, res) => {
   try {
     const user = await userUtils.findById(req.params.id, res);
@@ -21,18 +33,6 @@ router.get('/:id', verifyToken, async (req, res) => {
     const { nick, name, level, profileUrl } = user;
 
     return res.status(HTTP_STATUS_CODE.OK).json({ user: { nick, name, level, profileUrl } });
-  } catch {
-    return response.sendInternalError(res);
-  };
-});
-
-router.get('/my', verifyToken, async (req, res) => {
-  try {
-    const user = await userUtils.findById(req.id, res);
-    
-    const { nick, name, level, email, gender, profileUrl, verified } = user;
-
-    return res.status(HTTP_STATUS_CODE.OK).json({ user: { nick, name, level, email, gender, profileUrl, verified } });
   } catch {
     return response.sendInternalError(res);
   };
