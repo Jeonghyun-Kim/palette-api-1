f2f1a039c63aeef4c8dbce4fccc80e9784a25924
Kay Kim <kimjh@bawi.org>
2020-06-03 21:08:10 +0900

    add painting.js (paintingUtils)

---
+++ v2/utils/painting.js
@@ -0,0 +1,77 @@
+const logger = require('../../config/winston_config');
+const db = require('../../models');
+const { response } = require('./common');
+
+const paintingUtils = {};
+
+paintingUtils.findOne = async (id, res) => {
+  try {
+    return await db.Painting.findOne({ where: { id } });
+  } catch (err) {
+    logger.error(`[PAINTING-findOne] ${err}`);
+
+    return response.sendInternalError(res);
+  }
+};
+
+paintingUtils.create = async ({
+  painter, name, description, material, width, height,
+  price, onSale, numImages, thumbnailUrl,
+}, res) => {
+  try {
+    const painting = await db.Painting.create({
+      painter,
+      name,
+      description,
+      material,
+      width,
+      height,
+      price,
+      onSale,
+      numImages,
+      thumbnailUrl,
+    });
+
+    return { id: painting.id };
+  } catch (err) {
+    logger.error(`[PAINTING-create] ${err}`);
+
+    return response.sendInternalError(res);
+  }
+};
+
+paintingUtils.updateInfo = async (id, {
+  painter, name, description, material, width, height, price, onSale,
+}, res) => {
+  try {
+    const painting = await paintingUtils.findById(id, res);
+
+    await painting.update({ painter, name, description, material, width, height, price, onSale });
+
+    return painting;
+  } catch (err) {
+    logger.error(`[PAINTING-updateInfo] ${err}`);
+
+    return response.sendInternalError(res);
+  }
+};
+
+paintingUtils.delete = async (id, res) => {
+  try {
+    const painting = await paintingUtils.findOne(id, res);
+
+    if (!painting) {
+      return false;
+    }
+
+    await painting.destroy();
+
+    return true;
+  } catch (err) {
+    logger.error(`[PAINTING-delete] ${err}`);
+
+    return response.sendInternalError(res);
+  }
+};
+
+module.expurts = paintingUtils;
