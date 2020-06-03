f020f109e2cce3382ae63d08f5049bea3c8be7dd
Kay Kim <kimjh@bawi.org>
2020-06-03 22:39:52 +0900

    move post /gallery to post /admin/gallery

--- v2/routes/adminRouter.js
+++ v2/routes/adminRouter.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const sha256 = require('sha256');
+const { uuid } = require('uuidv4');
 const jsonParser = require('body-parser').json();
+const path = require('path');
+const multer = require('multer');
+
+const { response, uploadS3 } = require('../utils/common');
 const userUtils = require('../utils/user');
+const galleryUtils = require('../utils/gallery');
 const { verifyToken, checkAdmin } = require('../middlewares');
+const { BUCKETS } = require('../aws_defines');
 const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../../status_code');
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1000,
+  },
+});
 
 const router = express.Router();
 router.use(jsonParser);
@@ -77,6 +92,31 @@ router.delete('/user/:id', verifyToken, checkAdmin, async (req, res, next) => {
     }
 
     return res.status(HTTP_STATUS_CODE.BAD_REQUEST.json({ erorr: DB_STATUS_CODE.NO_SUCH_USER }));
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('/gallery', verifyToken, checkAdmin, upload.single('profile'), async (req, res, next) => {
+  const { name, address = '', tel, mobile, profileMsg = '' } = req.body;
+  if (!name && !tel && !mobile) {
+    return res.status(HTTP_STATUS_CODE.BAD_REQUEST)
+      .json({ error: DB_STATUS_CODE.GALLERY_BAD_REQUEST });
+  }
+  // TODO: Input Validation
+  try {
+    let profileUrl = null;
+    if (req.file) {
+      profileUrl = `${sha256(uuid())}${path.extname(req.file.originalname)}`;
+      if (!await uploadS3(BUCKETS.PROFILE, profileUrl, req.file.buffer)) {
+        return response.sendInternalError(res);
+      }
+    }
+    const { id } = await galleryUtils.create({
+      name, address, tel, mobile, profileMsg, profileUrl,
+    }, res);
+
+    return res.status(HTTP_STATUS_CODE.CREATED).json({ id });
   } catch (err) {
     return next(err);
   }
--- v2/routes/galleryRouter.js
+++ v2/routes/galleryRouter.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const sha256 = require('sha256');
-const { uuid } = require('uuidv4');
+// const sha256 = require('sha256');
+// const { uuid } = require('uuidv4');
 const jsonParser = require('body-parser').json();
-const path = require('path');
-const multer = require('multer');
+// const path = require('path');
+// const multer = require('multer');
 
-const { response, uploadS3 } = require('../utils/common');
+// const { response, uploadS3 } = require('../utils/common');
 // const userUtils = require('../utils/user');
 const galleryUtils = require('../utils/gallery');
-const { verifyToken, checkAdmin } = require('../middlewares');
-const { BUCKETS } = require('../aws_defines');
-const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../../status_code');
+const { verifyToken } = require('../middlewares');
+// const { BUCKETS } = require('../aws_defines');
+const { HTTP_STATUS_CODE } = require('../../status_code');
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 1000,
-  },
-});
+// const upload = multer({
+//   storage: multer.memoryStorage(),
+//   limits: {
+//     fileSize: 1000,
+//   },
+// });
 
 const router = express.Router();
 router.use(jsonParser);
@@ -65,31 +65,6 @@ router.get('/all', verifyToken, async (_req, res, next) => {
     const galleries = await galleryUtils.findAll(res);
 
     return res.status(HTTP_STATUS_CODE.OK).json({ galleries });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-router.post('/', verifyToken, checkAdmin, upload.single('profile'), async (req, res, next) => {
-  const { name, address = '', tel, mobile, profileMsg = '' } = req.body;
-  if (!name && !tel && !mobile) {
-    return res.status(HTTP_STATUS_CODE.BAD_REQUEST)
-      .json({ error: DB_STATUS_CODE.GALLERY_BAD_REQUEST });
-  }
-  // TODO: Input Validation
-  try {
-    let profileUrl = null;
-    if (req.file) {
-      profileUrl = `${sha256(uuid())}${path.extname(req.file.originalname)}`;
-      if (!await uploadS3(BUCKETS.PROFILE, profileUrl, req.file.buffer)) {
-        return response.sendInternalError(res);
-      }
-    }
-    const { id } = await galleryUtils.create({
-      name, address, tel, mobile, profileMsg, profileUrl,
-    }, res);
-
-    return res.status(HTTP_STATUS_CODE.CREATED).json({ id });
   } catch (err) {
     return next(err);
   }
